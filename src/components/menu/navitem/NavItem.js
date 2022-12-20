256712406f571e73fb34243a9bfe7a1798020c08
Bunyod <84246289+Bunyood@users.noreply.github.com>
2022-12-20 11:42:47 +0500

    add pages and order payment

--- src/components/menu/navitem/NavItem.js
+++ src/components/menu/navitem/NavItem.js
@@ -3,7 +3,7 @@ import './navitem.scss';
 
 export const NavItem = ({icon,link}) => {
   return (
-    <li>
+    <li className='nav-list-item'>
       <NavLink  className={({ isActive }) =>
               isActive ? "nav-item item-active" : "nav-item "
             }  to={link}>
